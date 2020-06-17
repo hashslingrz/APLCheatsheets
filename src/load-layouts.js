308e2a7624a6f1537a6ab9b681a660e729f2f1d8
Matt Wiese <matt@dyalog.com>
2020-06-17 14:55:30 +0100

    Simply JSON loading
    This simplifies importing the JSON into Svelte; however, I would like to fetch the JSON instead. This would allow easy integration of contributions from others without requiring a rebuild.

--- src/load-layouts.js
+++ src/load-layouts.js
@@ -4,26 +4,7 @@ import ucmd from '../data/ucmd.json';
 import shortcuts from '../data/shortcuts.json';
 
 let items = [
-	gridHelp.item({
-		x: 0,
-		y: 0,
-		w: 2,
-		h: 3,
-		id: ucmd.id,
-		title: ucmd.title,
-		header: ucmd.headers,
-		row: ucmd.rows
-	}),
-	gridHelp.item({
-		x: 2,
-		y: 0,
-		w: 2,
-		h: 6,
-		id: shortcuts.id,
-		static: true,
-		title: shortcuts.title,
-		header: shortcuts.headers,
-		row: shortcuts.rows
-	})
+	gridHelp.item(ucmd),
+	gridHelp.item(shortcuts)
 ];
 export { items };
